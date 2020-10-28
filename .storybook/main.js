63cc6ad9ce840b0ba985d83e65f77865d4565bdd
Ainsley Bernard <ains_bernard@hotmail.com>
2020-10-28 14:23:17 -0400

    adds stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,24 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-actions",
+    "@storybook/addon-knobs",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        babelOptions: {
+          presets: [
+            [
+              "@vue/cli-plugin-babel/preset",
+              {
+                jsx: false
+              }
+            ]
+          ]
+        }
+      }
+    }
   ]
-}
+};
---
+++ src/components/questionnaire/group/question/response/pure-text-response.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <v-text-field
+      ref="textControl"
+      v-model="value"
+      outlined
+      filled
+      dense
+      validate-on-blur
+      :rules="validationRulesFuncs"
+      @change="onChange($event)"
+      @update:error="onError($event)"
+    />
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  props: {
+    question: {
+      type: Object,
+      required: true
+    },
+    lang: {
+      type: String,
+      default: 'fr-FR'
+    }
+  },
+  data () {
+    return {
+      value: null,
+      validationRulesFuncs: [
+        value => {
+          if (this.question.isVisible) {
+            for (let i = 0; i < this.question.validationRules.length; i++) {
+              let ruleDefinition = this.question.validationRules[i]
+              if (!ruleDefinition.enabled) {
+                continue
+              }
+              let errorMessage = ruleDefinition.errorMessage[this.lang]
+              if (ruleDefinition.type === 'require') {
+                if (!value) {
+                  return errorMessage
+                }
+              } else if (ruleDefinition.type === 'minLength') {
+                if (!value || value.length < ruleDefinition.value) {
+                  return errorMessage.replace('{0}', ruleDefinition.value)
+                }
+              } else if (ruleDefinition.type === 'maxLength') {
+                if (!value || value.length > ruleDefinition.value) {
+                  return errorMessage.replace('{0}', ruleDefinition.value)
+                }
+              }
+            }
+          }
+          return true
+        }
+      ]
+    }
+  },
+  mounted () {
+    this.$watch(
+      '$refs.textControl.validations.length',
+      (newValue) => {
+        this.onError(!!newValue)
+      }
+    )
+
+    if (this.question.response != null) {
+      this.value = this.question.response
+      this.onChange()
+    }
+  },
+  methods: {
+    onChange () {
+      let args = {
+        value: this.value
+      }
+      if (!args.value) {
+        args.value = null
+      }
+
+      this.$emit('change', args)
+    },
+    onError (error) {
+      this.$emit('error', error)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
