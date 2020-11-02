ede48af3f3d5bac908bc46f09e4c5455309baec1
Ainsley Bernard <ains_bernard@hotmail.com>
2020-11-02 14:03:18 -0500

    adds a11y functionality to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
 import {addDecorator} from "@storybook/vue"
+import { withA11Y } from "@storybook/addon-a11y";
+
 import Vue from "vue"
 //Note: import from vuetify (non tree shakeable) makes it more simple then from importing vuetify-lib which is proper way to load the components we use for production, but for storybook/dev environment not needed, avoids additional setup/avoid install Veutify loader webpack.
 import Vuetify from "vuetify" 
@@ -8,10 +10,11 @@ import "@mdi/font/css/materialdesignicons.css";
 Vue.use(Vuetify, {
 })
 //note wraps stories in the v-app to properly display vuetify components
-addDecorator(()=> ({
-vuetify: new Vuetify({}),
-template: `<v-app><story/></v-app>`
-}))
+addDecorator(() => ({
+  vuetify: new Vuetify({}),
+  template: `<v-app><story/></v-app>`
+}));
+
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
