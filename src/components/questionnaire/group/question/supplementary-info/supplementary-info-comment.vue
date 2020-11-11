0ea39c615c0b9a115c6d86b39efc8d30fecc3a6c
lboleda <lboleda@hotmail.com>
2020-11-11 11:36:29 -0500

    Deleting Response and Question storers

--- src/components/questionnaire/group/question/supplementary-info/supplementary-info-comment.vue
+++ src/components/questionnaire/group/question/supplementary-info/supplementary-info-comment.vue
@@ -122,17 +122,8 @@ export default {
     )
   },
   methods: {
-
-    // TODO: think of a better name
-    updateResponseStore: function (response) {
-      const question = this.question
-      const group = this.group
-      const saveToProp = this.saveToProp
-      this.$store.dispatch('updateSupplementaryInfo', { saveToProp, group, question, response })
-    },
     handler: function (value) {
       // TODO: better name of argument.
-      this.updateResponseStore(value)
     },
     onError (error) {
       this.comment.validationStatus = !error
