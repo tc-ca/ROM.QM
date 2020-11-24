7ef977fcda37b96fa8ee15ed5e0398fcd98400d6
Ainsley Bernard <ains_bernard@hotmail.com>
2020-11-24 10:28:13 -0500

    refactors questionnaire.js in store module

--- src/store/modules/questionnaire.js
+++ src/store/modules/questionnaire.js
@@ -2,42 +2,44 @@ import { XrmWebApi } from "../../services/questionnaireService";
 import builderService from "../../services/builderService";
 
 export const state = {
-  questionnaire: null,
-  questionnaireId: null
+  questionnaire: null
 };
 export const actions = {
-  SetQuestionnaireIdState({ commit }, id) {
-    alert('inside set id state')
-    commit("setQuestionnaireId", id);
-  },
-
   SetQuestionnaireState({ commit, dispatch }, payload) {
     const { questionnaire, page, id } = payload;
     switch (page) {
       case "builder":
         {
-          alert('builder')
-          // does some processing before setting state
-          const {questionnaireData, groupsData} = builderService.processBuilderForSave(questionnaire);
+          // builder does some processing before setting state
+          const {
+            questionnaireData,
+            groupsData
+          } = builderService.processBuilderForSave(questionnaire);
           commit("setQuestionnaire", questionnaireData);
           dispatch("setQuestionnaireGroups", groupsData);
         }
         break;
       case "questionnaire":
-        alert('questionnaire')
-        commit("setQuestionnaire", questionnaire);
-        dispatch("setQuestionnaireGroups", questionnaire.groups);
+        {
+          alert("questionnaire");
+          commit("setQuestionnaire", questionnaire);
+          dispatch("setQuestionnaireGroups", questionnaire.groups);
+        }
+
         break;
       default:
         break;
     }
-              commit("setQuestionnaireId", id);
+    commit("setQuestionnaireId", id);
   },
 
   async SaveQuestionnaireStateToDynamics({ state }) {
     const questionnaire = state.questionnaire;
-    const questionnaireId = state.questionnaire.questionnaireId
-    const result = await XrmWebApi.SaveQuestionnaire(questionnaire, questionnaireId);
+    const questionnaireId = state.questionnaire.questionnaireId;
+    const result = await XrmWebApi.SaveQuestionnaire(
+      questionnaire,
+      questionnaireId
+    );
     return result;
   }
 };
@@ -45,14 +47,9 @@ export const actions = {
 export const mutations = {
   setQuestionnaire(state, payload) {
     state.questionnaire = payload;
-    alert('state looks like: '+JSON.stringify(state.questionnaire))
   },
 
   setQuestionnaireId(state, payload) {
-    //state.questionnaireId = payload;
-    alert("state looks like for id: " + state.questionnaireId);
-
     state.questionnaire.questionnaireId = payload;
-
   }
 };
