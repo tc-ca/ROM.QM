88ba4c0287d3e910633fa41797c23054ad35b3cc
Ainsley Bernard <ains_bernard@hotmail.com>
2020-11-27 16:56:24 -0500

    adds support to load legislations and beta questionnaire locally

--- src/store/modules/legislations.js
+++ src/store/modules/legislations.js
@@ -12,13 +12,15 @@ export const getters = {
 };
 
 export const actions = {
-  async SetLegislationsStateToLocalData({ commit }) {
-    const data = await GetLegislationFromLocalImportModule();
+  async SetTreeLegislationsStateToLocalData({ commit }) {
+    const data = await GetTreeLegislationFromLocalImportModule();
+    commit("SetLegislations", data);
+  },
+    async SetFlatLegislationsStateToLocalData({ commit }) {
+    const data = await GetFlatLegislationFromLocalImportModule();
     commit("SetLegislations", data);
   },
   async SetLegislationsState({ commit }, payload) {
-    //////////////////////
-    /////legislations is the name of the property on payload
     const { legislations } = payload
     commit("SetLegislations", legislations);
   }
@@ -30,8 +32,18 @@ export const mutations = {
   }
 };
 
-async function GetLegislationFromLocalImportModule() {
+async function GetTreeLegislationFromLocalImportModule() {
   //TODO 
+  //legislationDictionnary = render
+  //legislation = builder
+  const data = await import("../../api/legislation.json").then(module => {
+    return module.default;
+  });
+  return data;
+}
+
+async function GetFlatLegislationFromLocalImportModule() {
+  //TODO
   //legislationDictionnary = render
   //legislation = builder
   const data = await import("../../api/legislationDictionnary").then(module => {
